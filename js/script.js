909b6c42e7ce2ff262e538cdbc3fc5ad1cf04ff5
= <giacomo.bartoli@me.com>
2024-02-28 18:58:20 +0100

    add animation header

--- js/script.js
+++ js/script.js
@@ -1,30 +1,64 @@
+const header = document.querySelector('header');
+const hero = document.getElementById('hero');
+const observered = document.querySelector('.observed'); 
+const navbar = document.querySelector('ul.navbar-nav')
+const searchbar = document.querySelector('.searchbar');
+const searchbarInputs = document.querySelectorAll('.searchbar input');
+const searchbarButton = document.querySelector('.searchbar__button');
+
+
+window.addEventListener('scroll', ()=> {
+    if(this.innerWidth >= 1024) {
+        searchbarAnimation();
+
+        if (this.scrollY >= (hero.clientHeight)) {
+            changeHeader(true)
+        } else {
+            changeHeader(false)
+        }
+    }
+})
+
+
 const searchbarAnimation = () => {
-    window.addEventListener('scroll', ()=> {
-        if(this.scrollY > document.querySelector('header').clientHeight) {
-            document.querySelector('ul.navbar-nav').classList.add('scale-0');
-            document.querySelectorAll('.searchbar input').forEach(input => {
-                input.classList.add('d-none')
-            })
-            document.querySelector('.searchbar').classList.add('searchbar--small');
-            document.querySelector('.searchbar__button').classList.add('searchbar__button--small');
-        }
-        else {
-            document.querySelector('ul.navbar-nav').classList.remove('scale-0');
-            document.querySelectorAll('.searchbar input').forEach(input => {
-                input.classList.remove('d-none')
-            })
-    
-            document.querySelector('.searchbar').classList.remove('searchbar--small');
-            document.querySelector('.searchbar__button').classList.remove('searchbar__button--small');     
-        }
-    })
-}
 
-if(this.innerWidth >= 1024) {
-    document.querySelector('header').classList.add('sticky-top','top-0');
-    searchbarAnimation();
-} else {
-    document.querySelector('header').classList.remove('sticky-top','top-0');
+    if(this.scrollY > header.clientHeight) {
+        navbar.classList.add('scale-0');
+        searchbarInputs.forEach(input => {
+            input.classList.add('d-none')
+        })
+        searchbar.classList.add('searchbar--small', 'shadow');
+        searchbarButton.classList.add('searchbar__button--small');
+    }
+    else {
+        navbar.classList.remove('scale-0');
+        searchbarInputs.forEach(input => {
+            input.classList.remove('d-none')
+        })
+        searchbar.classList.remove('searchbar--small', 'shadow');
+        searchbarButton.classList.remove('searchbar__button--small');     
+    }
 }
 
 
+function changeHeader(bool) {
+    let isVisible = bool;
+    const logo = document.querySelector('.logo--white');
+    const menu = document.querySelectorAll('.settings-nav a.nav-link');
+
+    if (isVisible) {
+        header.classList.add('bg-white', 'shadow-sm');
+        logo.src = './assets/logo-pink.png';
+        menu.forEach(link => {
+            link.classList.remove('text-white')
+            link.classList.add('text-secondary')
+        })
+    } else {
+        header.classList.remove('bg-white', 'shadow-sm');
+        logo.src = './assets/logo.png';
+        menu.forEach(link => {
+            link.classList.add('text-white')
+            link.classList.remove('text-secondary')
+        })
+    }
+}
